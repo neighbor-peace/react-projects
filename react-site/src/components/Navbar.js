e5b15067430569f0c6b790cea67a8bb7a8483625
Michael Costello <mcostello91@gmail.com>
2022-07-01 17:52:20 -0700

    Style navbar

--- react-site/src/components/Navbar.js
+++ react-site/src/components/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import logo from "../logo.svg";
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img alt="react logo" className="nav--icon" src={logo} />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
