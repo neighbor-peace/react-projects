b13795310dddd12157ccb436c0155236bfcf06e4
Michael Costello <mcostello91@gmail.com>
2022-07-04 16:51:31 -0700

    Style navbar and hero

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,4 +1,6 @@
 import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 /*
 Challenge: Build the Navbar component.
@@ -7,6 +9,10 @@ Check the Figma file for the design specifics.
 
 export default function App() {
     return (
-        <h1>App component</h1>
+        <div>
+          <Navbar />
+          <Hero />
+
+        </div>
     )
 }
